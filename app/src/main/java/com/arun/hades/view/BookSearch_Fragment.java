d0ae7bc7d97d7ec4ae5d8aa03c8bac82acef9942
Arun <akumar@krypton-solutions.com>
2021-01-06 17:46:34 +0530

    Change File

--- app/src/main/java/com/arun/hades/view/BookSearch_Fragment.java
+++ app/src/main/java/com/arun/hades/view/BookSearch_Fragment.java
@@ -7,27 +7,24 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.arun.hades.viewmodel.BookSearchViewModel;
 import com.arun.hades.R;
 
 public  class BookSearch_Fragment extends Fragment {
 
 
-    private BookSearchViewModel mViewModel;
+    private BookSearchModel mViewModel;
     private ViewDataBinding binding;
-
+public BookSearch_Fragment(){}
     public static BookSearch_Fragment newInstance() { return new BookSearch_Fragment(); }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        mViewModel = new ViewModelProvider(this).get(BookSearchViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(BookSearchModel.class);
         super.onCreate(savedInstanceState);
 
 
@@ -38,9 +35,6 @@ public  class BookSearch_Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
          super.onCreateView(inflater, container, savedInstanceState);
          binding = DataBindingUtil.inflate(inflater, R.layout.booksearch__fragment, container, false);
-        //binding.setLifecycleOwner(this);
-        binding.bookSearchViewmodel=mViewModel;
-
         return binding.getRoot();
     }
 
